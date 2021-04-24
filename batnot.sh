6dee210ca29fd2dfafba89cba20f7141e5c2098b
kolobok kelbek <dev_kelbek@mail.ru>
2021-04-24 15:21:26 +0300

    refactored script and added icons

--- batnot.sh
+++ batnot.sh
@@ -1,44 +1,54 @@
 #!/bin/bash
 
-prev=100;
+prev_percentage=100
 percentage=72
-low=0
-normal=0
-critical=0
+
 die=0
-full=0
+critical=0
+low=0
+middle=0
+high=0
+
+if [ -z $BATNOT_ICONS_PATH ]; then
+      icons_path=$BATNOT_ICONS_PATH
+else
+      icons_path=/usr/share/icons/batnot
+fi
+
+icon_low_level=$BATNOT_ICONS_PATH/low-level.svg
+icon_middle_level=$BATNOT_ICONS_PATH/middle-level.svg
+icon_high_level=$BATNOT_ICONS_PATH/high-level.svg
 
 while [ 1 = 1 ]
 do
     percentage=$(upower -i $(upower -e | grep 'BAT') | grep -E "percentage" | grep -Eo "[0-9]{1,4}")
 
-    if [ $percentage -lt $prev ]; then
+    if [ $percentage -lt $prev_percentage ]; then
         if [ $percentage -le 5 ] && [ $die -le 0 ]; then
-            notify-send -u critical -i /usr/share/icons/Adwaita/scalable/status/battery-level-0-symbolic.svg "low battery $percentage%, charging required"
+            notify-send -u critical -i $icon_low_level "Low battery $percentage%, charging required"
             die=1
         elif [ $percentage -le 15 ] && [ $percentage -gt 5 ] && [ $critical -le 0 ]; then
-            notify-send -u critical -i /usr/share/icons/Adwaita/scalable/status/battery-level-0-symbolic.svg "low battery $percentage%, charging required"
+            notify-send -u critical -i $icon_low_level "Low battery $percentage%, charging required"
             critical=1
-        elif [ $percentage -le 30 ] && [ $percentage -gt 15 ] && [ $normal -eq 0 ]; then
-            notify-send -u normal -i /usr/share/icons/Adwaita/scalable/status/battery-level-10-symbolic.svg "low battery $percentage%"
-            normal=1
-        elif [ $percentage -le 50 ] && [ $percentage -gt 30 ] && [ $low -eq 0 ]; then
-            notify-send -u low -i /usr/share/icons/Adwaita/scalable/status/battery-level-50-charging-symbolic.svg "$percentage% battery"
+        elif [ $percentage -le 30 ] && [ $percentage -gt 15 ] && [ $low -eq 0 ]; then
+            notify-send -u normal -i $icon_middle_level "Low battery $percentage%"
             low=1
-        elif [ $percentage -le 70 ] && [ $percentage -gt 50 ] && [ $full -eq 0 ]; then
-            notify-send -u low -i /usr/share/icons/Adwaita/scalable/status/battery-level-50-charging-symbolic.svg "$percentage% battery"
-            full=1
+        elif [ $percentage -le 50 ] && [ $percentage -gt 30 ] && [ $middle -eq 0 ]; then
+            notify-send -u low -i $icon_high_level "$percentage% battery"
+            middle=1
+        elif [ $percentage -le 70 ] && [ $percentage -gt 50 ] && [ $high -eq 0 ]; then
+            notify-send -u low -i $icon_high_level "$percentage% battery"
+            high=1
         fi;
-    elif [ $percentage -gt $prev ]; then
+    elif [ $percentage -gt $prev_percentage ]; then
         die=0
-        low=0
-        normal=0
         critical=0
-        test=0
-        full=0
+        low=0
+        middle=0
+        high=0
     fi;
 
-    prev=$percentage
+    prev_percentage=$percentage
 
     sleep 1s
 done
